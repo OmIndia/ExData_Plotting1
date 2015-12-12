217a741d233296e4e1a243a4c042bdb12821ce10
Om <chand_kw6@yahoo.com>
2015-12-12 14:31:46 +0530

    Om Project 1-updated

--- plot3.R
+++ plot3.R
@@ -22,7 +22,7 @@ with(pow_consume_final,{
       lines(newdate, pow_consume_final$Sub_metering_1, col = "black")
       lines(newdate, pow_consume_final$Sub_metering_2, col = "red")
       lines(newdate, pow_consume_final$Sub_metering_3, col = "blue")
-      legend("topright", pch = c("-"), col = c("black","red","blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+      legend("topright", lty=1, col = c("black","red","blue"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
        })
 
 dev.off()
